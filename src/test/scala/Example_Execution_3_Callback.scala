6cf22ae3f9f25fc7b05323a692d75653c722962a
jeffwji <and3b6s5y>
2019-09-29 09:04:16 -0400

    Add cancelable and callback

--- src/test/scala/Example_Execution_3_Callback.scala
+++ src/test/scala/Example_Execution_3_Callback.scala
@@ -5,6 +5,8 @@ import org.scalatest.FlatSpec
 class Example_Execution_3_Callback extends FlatSpec with StrictLogging {
     "Callback" should "" in {
         /**
+         * https://monix.io/docs/3x/execution/callback.html
+         *
          * Callback 和 IO 的 async 类似，它可以给 Task 在结束的时候附加一个回调函数。这个回调函数通过协程框架来处理。
          * 不同的是 Monix 的 callback 可以定义左右值的数据类型, 不想 IO Async 的左值必须是 Throwable
          */
@@ -24,5 +26,18 @@ class Example_Execution_3_Callback extends FlatSpec with StrictLogging {
 
         import monix.execution.Scheduler.Implicits.global
         task.runAsync(callback)
+
+        // 需要注意的是callback 不是 stack 安全的：https://monix.io/docs/3x/execution/callback.html
+    }
+
+    "Promise to Callback" should "" in {
+        /**
+         * 可以将一个 Promise 转换成 Callback
+         */
+        val p = scala.concurrent.Promise[String]()
+        // p: scala.concurrent.Promise[String] = Future(<not completed>)
+
+        val callback = Callback.fromPromise(p)
+        // callback: monix.execution.Callback[Throwable,String] = <function1>
     }
 }
